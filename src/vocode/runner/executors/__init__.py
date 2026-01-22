2ce7184a54dd693d0ed97b257fc062d575b18145
Serge Koval <serge.koval+github@gmail.com>
2026-01-22 15:10:04 -0500

    HTTP chat example

--- src/vocode/runner/executors/__init__.py
+++ src/vocode/runner/executors/__init__.py
@@ -1,1 +1,10 @@
 from . import exec_agent
+from . import http_input
+from . import llm
+from . import input
+from . import noop
+from . import apply_patch_node
+from . import exec_node
+from . import file_read
+from . import result
+
