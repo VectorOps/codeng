68c19254488c7f5968ea84ffaaca52ebd148d91c
Serge Koval <serge.koval+github@gmail.com>
2026-04-14 19:35:59 -0400

    Do not fail task on hard LLM failures

--- src/vocode/manager/base.py
+++ src/vocode/manager/base.py
@@ -287,9 +287,14 @@ class BaseManager:
                     else:
                         event = await agen.asend(send)
                 except StopAsyncIteration:
-                    # If runner generator finished, pop it from the stack
-                    finished_frame = self._runner_stack.pop()
+                    finished_frame = self._runner_stack[-1]
                     finished_frame.agen = None
+
+                    if finished_frame.runner.status == state.RunnerStatus.STOPPED:
+                        self.project.current_workflow = finished_frame.workflow_name
+                        return
+
+                    self._runner_stack.pop()
 
                     # If we still have the runners in the stack, then pass back last final message
                     final_message = finished_frame.runner.last_final_message
